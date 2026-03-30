1f781cd14cb9bdebc299dc12aa3c7560fa282003
Nam Dao <262199921+metzgerdev@users.noreply.github.com>
2026-03-30 16:29:26 -0700

    perf: Cache static assets

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./Style/index.css";
+import { registerAssetCache } from "./utils/registerAssetCache";
 
 const container = document.getElementById("root");
 
@@ -16,3 +17,5 @@ root.render(
     <App />
   </React.StrictMode>,
 );
+
+void registerAssetCache();
